0e8d9d5a84c245b3e594ae606a86127a47911c5c
Tamas Levente Kis <tamas.levente.kiss@ericsson.com>
2025-04-22 09:41:25 +0200

    changes

---
+++ readme.md
@@ -0,0 +1,8 @@
+# rspt_module
+
+2D array squaring module in C++ using pybind11.
+
+## Install
+
+```bash
+pip install git+https://github.com/tamask1s/rspt_module.git
--- setup.py
+++ setup.py
@@ -1,25 +1,17 @@
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
 import numpy
+import pybind11
 
-ext_modules = [
-    Extension(
-        "rspt_module",
-        ["rspt_module/rspt_module.cpp"],
-        include_dirs=[
-            numpy.get_include(),
-            # pybind11 include dir-t majd a setup_requires biztosítja
-        ],
-        language="c++"
-    ),
-]
+module = Extension(
+    'rspt_module',
+    sources=['rspt_module/rspt_module.cpp'],
+    include_dirs=[numpy.get_include(), pybind11.get_include()],
+    language='c++'
+)
 
 setup(
-    name="rspt_module",
-    version="0.1",
-    author="tamask1s",
-    description="2D array squaring functions in C++",
-    ext_modules=ext_modules,
-    install_requires=["numpy"],
-    setup_requires=["pybind11>=2.5.0"],
-    zip_safe=False,
+    name='rspt_module',
+    version='0.1',
+    description='2D array squaring functions in C++',
+    ext_modules=[module]
 )
