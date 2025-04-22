eff8f39e2a3838cf6b8384edbbb7cf2d0b20d5f4
Tamas Levente Kis <tamas.levente.kiss@ericsson.com>
2025-04-22 09:19:13 +0200

    changes

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from skbuild import setup
+from setuptools import setup, Extension
+import pybind11
+import numpy
+
+ext_modules = [
+    Extension(
+        "rspt_module",
+        ["rspt_module/rspt_module.cpp"],
+        include_dirs=[
+            pybind11.get_include(),
+            numpy.get_include(),
+        ],
+        language="c++"
+    ),
+]
 
 setup(
     name="rspt_module",
     version="0.1",
     author="tamask1s",
     description="2D array squaring functions in C++",
-    packages=["rspt_module"],
-    python_requires=">=3.6",
+    ext_modules=ext_modules,
+    install_requires=["numpy", "pybind11"],
+    zip_safe=False,
 )
