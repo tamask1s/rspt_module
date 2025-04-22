42afe072b47830fa3206c5ec64bf09e79dbac593
Tamas Levente Kis <tamas.levente.kiss@ericsson.com>
2025-04-22 09:46:15 +0200

    changes

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import numpy
-import pybind11
+
+# pybind11 include dir-t csak build időben töltjük be
+try:
+    import pybind11
+    pybind11_include = pybind11.get_include()
+except ImportError:
+    pybind11_include = ""
 
 module = Extension(
     'rspt_module',
     sources=['rspt_module/rspt_module.cpp'],
-    include_dirs=[numpy.get_include(), pybind11.get_include()],
+    include_dirs=[numpy.get_include(), pybind11_include],
     language='c++'
 )
 
@@ -13,5 +19,7 @@ setup(
     name='rspt_module',
     version='0.1',
     description='2D array squaring functions in C++',
-    ext_modules=[module]
+    ext_modules=[module],
+    install_requires=['numpy'],
+    setup_requires=['pybind11>=2.5.0'],
 )
